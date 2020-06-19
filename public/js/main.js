e2b1f781535f7f1aecaa38af4b84ee979dcd36e3
Leonardo Modena <leonardo.modena@marconirovereto.it>
2020-06-19 13:42:17 +0200

    yo

--- public/js/main.js
+++ public/js/main.js
@@ -3,8 +3,6 @@ $(document).ready(function() {
         //options here
         autoScrolling: true,
         licenseKey: '12A17E0E-01C34A10-AA62E6F2-562D6154',
-        'navigation': true,
-        'navigationPosition': 'right',
 
         onLeave: function(index, nextIndex, direction) {
             //after leaving section 2
@@ -53,4 +51,14 @@ $(document).ready(function() {
 document.querySelector('#moveAboutMe').addEventListener('click', function(e) {
     e.preventDefault();
     fullpage_api.moveTo(2);
+});
+
+document.querySelector('#moveMyWorks').addEventListener('click', function(e) {
+    e.preventDefault();
+    fullpage_api.moveTo(3);
+});
+
+document.querySelector('#drasticLogo').addEventListener('click', function(e) {
+    e.preventDefault();
+    fullpage_api.moveTo(1);
 });
