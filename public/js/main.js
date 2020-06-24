42a795c1544759c95d272d221ddb1fc1a6d5a0fb
Leonardo Modena <leonardo.modena@marconirovereto.it>
2020-06-24 19:26:46 +0200

    ci siamo quasi

--- public/js/main.js
+++ public/js/main.js
@@ -6,27 +6,31 @@ $(document).ready(function() {
 
         onLeave: function(index, nextIndex, direction) {
             //after leaving section 2
-            if (index.index == 0 && direction == 'down') {
+            if (index.index === 0 && direction === 'down') {
                 $('.card').removeClass("moveUpText").addClass("moveDownText");
                 $('.card-photo').removeClass("moveDownPhoto").addClass("moveUpPhoto");
-            } else if (index.index == 1 && direction == 'up') {
+            } else if (index.index === 1 && direction === 'up') {
                 $('.card').removeClass("moveDownText").addClass("moveUpText");
                 $('.card-photo').removeClass("moveUpPhoto").addClass("moveDownPhoto");
             }
 
-            if (index.index == 1 && direction == 'down') {
-                $('.project').hide();
+            if (index.index === 1 && direction === 'down') {
+                $('#slide1').css("background-color", "unset");
                 $('.card-photo').addClass("fallingBall");
                 setTimeout(() => {
                     $('.card-photo').addClass("staticBall");
                 }, 480);
                 setTimeout(() => {
                     $('.card-photo').addClass("openBall");
-                }, 680);
+                }, 600);
+                setTimeout(() => {
+                    $('#slide1').css("background-color", "#0d0206a8");
+
+                }, 1200);
                 setTimeout(() => {
                     $('.card-photo').addClass("opacity0");
                     $('.project').show();
-                }, 1480);
+                }, 1400);
                 setTimeout(() => {
                     $('.card-photo').hide();
                     $('.card-photo').addClass("opacity1");
@@ -34,19 +38,29 @@ $(document).ready(function() {
                     $('.card-photo').removeClass('staticBall');
                     $('.card-photo').removeClass('fallingBall');
 
-                }, 2581);
+                }, 1401);
                 setTimeout(() => {
                     $('.card-photo').show();
                     $('.card-photo').removeClass('opacity0');
                     $('.card-photo').addClass("opacity1");
-                }, 3281);
-            } else if (index.index == 2 && direction == 'up') {
-
+                }, 1451);
+            } else if (index.index === 2 && direction === 'up') {
+                $('#slide1').css("background-color", "unset");
             }
 
         },
     });
+
+    $('#video-background').vide('/vid/background.mp4', {
+        muted: true,
+        loop: true,
+        autoplay: true,
+        resizing: true,
+        bgColor: 'transparent',
+    });
+
 });
+
 
 document.querySelector('#moveAboutMe').addEventListener('click', function(e) {
     e.preventDefault();
