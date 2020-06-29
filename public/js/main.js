2c039c7a18e1f9b8a66105fa3faf5a2278cdaec8
Leonardo Modena <leonardo.modena@marconirovereto.it>
2020-06-29 16:47:22 +0200

    finito

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,20 @@
+let dropdown = false;
+let contact = false;
+let mobile;
+
 $(document).ready(function() {
+
+    if (screen.width < 1025) {
+        mobile = true;
+    } else
+        mobile = false;
+
     $('#fullpage').fullpage({
         //options here
         autoScrolling: true,
         licenseKey: '12A17E0E-01C34A10-AA62E6F2-562D6154',
-
+        scrollingSpeed: 1000,
+        slidesNavigation: true,
         onLeave: function(index, nextIndex, direction) {
             //after leaving section 2
             if (index.index === 0 && direction === 'down') {
@@ -15,37 +26,42 @@ $(document).ready(function() {
             }
 
             if (index.index === 1 && direction === 'down') {
-                $('#slide1').css("background-color", "unset");
-                $('.card-photo').addClass("fallingBall");
-                setTimeout(() => {
-                    $('.card-photo').addClass("staticBall");
-                }, 480);
-                setTimeout(() => {
-                    $('.card-photo').addClass("openBall");
-                }, 600);
-                setTimeout(() => {
-                    $('#slide1').css("background-color", "#0d0206a8");
+                if (!mobile) {
+                    $('.project-card').css("background-color", "unset");
+                    $('.card-photo').addClass("fallingBall");
+                    setTimeout(() => {
+                        $('.card-photo').addClass("staticBall");
+                    }, 480);
+                    setTimeout(() => {
+                        $('.card-photo').addClass("openBall");
+                    }, 600);
+                    setTimeout(() => {
+                        $('.project-card').css("background-color", "#0d0206a8");
 
-                }, 1200);
-                setTimeout(() => {
-                    $('.card-photo').addClass("opacity0");
-                    $('.project').show();
-                }, 1400);
-                setTimeout(() => {
-                    $('.card-photo').hide();
-                    $('.card-photo').addClass("opacity1");
-                    $('.card-photo').removeClass('openBall');
-                    $('.card-photo').removeClass('staticBall');
-                    $('.card-photo').removeClass('fallingBall');
+                    }, 1200);
+                    setTimeout(() => {
+                        $('.card-photo').addClass("opacity0");
+                        $('.project').show();
+                    }, 1400);
+                    setTimeout(() => {
+                        $('.card-photo').hide();
+                        $('.card-photo').addClass("opacity1");
+                        $('.card-photo').removeClass('openBall');
+                        $('.card-photo').removeClass('staticBall');
+                        $('.card-photo').removeClass('fallingBall');
 
-                }, 1401);
-                setTimeout(() => {
-                    $('.card-photo').show();
-                    $('.card-photo').removeClass('opacity0');
-                    $('.card-photo').addClass("opacity1");
-                }, 1451);
+                    }, 1401);
+                    setTimeout(() => {
+                        $('.card-photo').show();
+                        $('.card-photo').removeClass('opacity0');
+                        $('.card-photo').addClass("opacity1");
+                    }, 1451);
+                } else {
+                    $('.project-card').css("background-color", "#0d0206a8");
+                }
+
             } else if (index.index === 2 && direction === 'up') {
-                $('#slide1').css("background-color", "unset");
+                $('.project-card').css("background-color", "unset");
             }
 
         },
@@ -75,4 +91,26 @@ document.querySelector('#moveMyWorks').addEventListener('click', function(e) {
 document.querySelector('#drasticLogo').addEventListener('click', function(e) {
     e.preventDefault();
     fullpage_api.moveTo(1);
-});
+})
+
+$("#dropdown-btn").click(function() {
+    if (!dropdown) {
+        $('#dropdown-btn').addClass("is-active");
+        $('.dd-menu').addClass('dd-menu-active');
+        dropdown = !dropdown;
+    } else {
+        $('#dropdown-btn').removeClass("is-active")
+        $('.dd-menu').removeClass('dd-menu-active');
+        dropdown = !dropdown;
+    }
+})
+
+$("#contactMe").click(function() {
+    if (!contact) {
+        $('.dd-menu').addClass('dd-menu-active');
+        contact = !contact;
+    } else {
+        $('.dd-menu').removeClass('dd-menu-active');
+        contact = !contact;
+    }
+})
